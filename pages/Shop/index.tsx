fbf9a66cbd7d62ccd49aa5f75b9dc995e1d582a6
investgod <110871245+investgod@users.noreply.github.com>
2024-04-28 08:30:05 -0500

    More changes bro

--- pages/Shop/index.tsx
+++ pages/Shop/index.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
+import styles from "../../styles/Shop.module.css";
 
-
-
-export default function Shop () {
+export default function Shop() {
     return (
-        <div>
-            <h1>Coming Soon</h1>
+        <div className={`${styles.shopContainer} flex items-center justify-center h-screen`}>
+            <header className='text-4xl font-bold transition-all duration-300 transform hover:scale-105'>
+                <h1 className={`${styles.shopHeader} animate-bounce`}>Coming Soon....</h1>
+            </header>
         </div>
     )
 }
