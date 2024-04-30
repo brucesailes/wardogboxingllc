8d2bfbfe7415b1feb4f8830ea459a3e17273dadb
investgod <110871245+investgod@users.noreply.github.com>
2024-04-30 06:37:08 -0500

    Yeeetttt

--- pages/Socials/index.tsx
+++ pages/Socials/index.tsx
@@ -11,7 +11,7 @@ export default function Socials(): ReactElement {
                 <Image src="/wardogboxing.png" alt="War Dog Boxing Logo" width={500} height={500} />
                 <h1 className="text-blue-600 text-center text-3xl font-bold m-4">CONNECT WITH US:</h1>
                 <ul className="mt-4 list-none p-0 text-center">
-                    <li className="mb-2"><a href="https://www.instagram.com/wardogboxing/?igsh=ZmV3c2tvMTl3NzVu" target="_blank" rel='noopener' className="text-purple-500 hover:text-blue-700"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
+                    <li className="mb-2"><a href="https://www.instagram.com/wardogboxing/?igsh=ZmV3c2tvMTl3NzVu" target="_blank" rel='noopener' className="text-purple-500"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
 
 
                     <li className="mb-2"><a href="https://www.tiktok.com/@war.dog.boxing?_t=8lu6lA9PccA&_r=1" target="_blank" rel='noopener' className="text-black"><FontAwesomeIcon icon={faTiktok} /> TikTok</a></li>
