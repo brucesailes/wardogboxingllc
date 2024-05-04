df01973507805224910b718d294fb544bb8e1ae0
investgod <110871245+investgod@users.noreply.github.com>
2024-05-03 20:01:22 -0500

    lets see

--- components/HomeSlideShow.tsx
+++ components/HomeSlideShow.tsx
@@ -5,22 +5,22 @@ export default function HomeSlideShow() {
   return (
     <div>
       <div className="text-center">
-        <h1 className={`${styles.gradientHeader} mx-4 mt-4 mb-2 text-xl sm:text-3xl lg:text-4xl font-bold transition-all duration-300 transform hover:scale-105`}>
+        <h1 className={`${styles.gradientHeader} mx-4 mt-4 mb-2 text-3xl sm:text-3xl lg:text-4xl font-bold transition-all duration-300 transform hover:scale-105`}>
           MAKE THE CHANGE OF A LIFETIME
         </h1>
       </div>
 
       <div className="grid grid-cols-3 gap-4 px-4 sm:px-6 lg:px-8">
         <div className="p-2 transition-all duration-300 transform hover:scale-105">
-          <Image src="/slide1.png" alt="Image 1" width={300} height={300} />
+          <Image src="/slide1.png" alt="Image 1" width={300} height={300} layout="responsive"/>
         </div>
 
         <div className="p-2 transition-all duration-300 transform hover:scale-105">
-          <Image src="/slide2.png" alt="Image 2" width={300} height={300} />
+          <Image src="/slide2.png" alt="Image 2" width={300} height={300} layout="responsive" />
         </div>
 
         <div className="p-2 transition-all duration-300 transform hover:scale-105">
-          <Image src="/slide3.png" alt="Image 3" width={300} height={300} />
+          <Image src="/slide3.png" alt="Image 3" width={300} height={300} layout="responsive" />
         </div>
 
       </div>
