6ffff46d1d347acba44ed6ef6b7768b364119e25
investgod <110871245+investgod@users.noreply.github.com>
2024-05-07 12:02:57 -0500

    CHANGES

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'; 
 import { companyInfo } from "../src/data/companyInfo";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
 import styles from '../styles/About.module.css';
 
 
 export default function About() {
     return (
-        <main className="flex flex-col  text-center text-blue-500 items-center justify-center min-h-screen mt-20  transition-all duration-300 transform hover:scale-105">
+        <main className="flex flex-col  text-center text-blue-500 items-center justify-center min-h-screen mt-20">
             <header className="text-center mb-8">
                 <h1 className={`${styles.gradientHeader} text-4xl mt-5 md:text-6xl font-bold  transition-all duration-300 transform hover:scale-105`}>WAR DOG BOXING</h1>
                 <blockquote className="text-l md:text-xl  mt-4  transition-all duration-300 transform hover:scale-105">&rdquo;{companyInfo.companySlogan}&rdquo;
@@ -14,7 +17,7 @@ export default function About() {
             </header>
             <section className="w-full max-w-4xl mx-auto p-8 rounded-xl shadow-xl space-y-6">
                 <div className="flex justify-center items-center p-6">
-                    <div className="w-64 h-64 md:w-96 md:h-96 relative overflow-hidden rounded-full drop-shadow-lg">
+                    <div className="w-64 h-64 md:w-96 md:h-96 relative overflow-hidden rounded-full drop-shadow-lg transition-all duration-300 transform hover:scale-105">
                         {/* Image component, responsive sizing */}
                         <Image 
                             src="/wardogboxing.png"
@@ -29,12 +32,11 @@ export default function About() {
                     OUR MISSION:
                 </h2>
 
-                <div className='flex justify-center items-center text-center text-l md:text-xl text-center leading-relaxed'>
-                    <p className="">
+                <div className='flex justify-center items-center text-center text-l md:text-xl text-center leading-relaxed transition-all duration-300 transform hover:scale-105'>
+                    <p>
                         {companyInfo.aboutText}
                     </p>
                 </div>
-
             </section>
         </main>
     );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,43 +6,36 @@ import Link from 'next/link';
 const Footer: React.FC = () => {
   return (
     <footer className="py-4 bg-gray-200 mt-auto text-blue-500">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="text-l text-center md:text-left mb-4 md:mb-0">
-          <p>
-            COPYRIGHT © 2024 War Dog Boxing - ALL RIGHTS RESERVED.
-          </p>
-          <ul className="flex justify-center md:justify-start mt-6 list-none p-4">
-            <li className="mx-2">
-              <Link href="https://www.instagram.com/wardogboxing/?igshid=1sewkk192ybyg" legacyBehavior>
-                <a target="_blank" rel="noopener noreferrer" className="text-purple-500 text-2xl">
-                  <FontAwesomeIcon icon={faInstagram} className="animate-bounce" />
-                </a>
-              </Link>
-            </li>
-            <li className="mx-2">
-              <Link href="https://www.tiktok.com/@war.dog.boxing?" legacyBehavior>
-                <a target="_blank" rel="noopener noreferrer" className="text-black text-2xl">
-                  <FontAwesomeIcon icon={faTiktok} className="animate-bounce" />
-                </a>
-              </Link>
-            </li>
-            <li className="mx-2">
-              <Link href="https://www.youtube.com/@WarDogBoxing" legacyBehavior>
-                <a target="_blank" rel="noopener noreferrer" className="text-red-500 text-2xl">
-                  <FontAwesomeIcon icon={faYoutube} className="animate-bounce" />
-                </a>
-              </Link>
-            </li>
-          </ul>
+      <div className="container mx-auto flex flex-col items-center">
+        <div className="flex justify-center space-x-4 mb-4">
+          <Link href="https://www.instagram.com/wardogboxing/?igshid=1sewkk192ybyg" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer" className="text-purple-500 text-2xl">
+              <FontAwesomeIcon icon={faInstagram} className="animate-bounce" />
+            </a>
+          </Link>
+          <Link href="https://www.tiktok.com/@war.dog.boxing?" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer" className="text-black text-2xl">
+              <FontAwesomeIcon icon={faTiktok} className="animate-bounce" />
+            </a>
+          </Link>
+          <Link href="https://www.youtube.com/@WarDogBoxing" legacyBehavior>
+            <a target="_blank" rel="noopener noreferrer" className="text-red-500 text-2xl">
+              <FontAwesomeIcon icon={faYoutube} className="animate-bounce" />
+            </a>
+          </Link>
         </div>
-        <div className="text-l text-center md:text-right">
-          <p>
-            Developed By: <a href="https://www.brucesailes.com" target="_blank" rel="noopener" className="underline">Bruce Sailes</a>
-          </p>
-        </div>
+        <p className="text-l text-center mb-4">
+          COPYRIGHT © 2024 War Dog Boxing - ALL RIGHTS RESERVED.
+        </p>
+        <p className="text-l text-center">
+          Developed By: <a href="https://www.brucesailes.com" target="_blank" rel="noopener" className="underline">Bruce Sailes</a>
+        </p>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
+
+
