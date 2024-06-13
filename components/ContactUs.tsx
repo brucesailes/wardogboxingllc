41141cca7d775ecc65851fc1e1b514cc8c557eed
investgod <110871245+investgod@users.noreply.github.com>
2024-06-13 06:31:32 -0500

    Yes sir

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
 
-
 const ContactUs: React.FC = () => {
   return (
     <section className="relative text-white py-16">
@@ -10,27 +9,32 @@ const ContactUs: React.FC = () => {
           src="/slide6.png" 
           alt="Background Image" 
           fill={true} 
-          style={{ objectFit: "cover" }} 
-          quality={50} 
+          style={{ objectFit: "cover", objectPosition: "center" }} 
+          quality={75} 
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           placeholder="blur"
-          blurDataURL="/slide6.png"
+          blurDataURL="/slide6-blur.png" // Use a low-quality placeholder for faster initial load
           loading="lazy"
         />
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <div className="relative container mx-auto flex flex-col md:flex-row items-center justify-center px-4">
-        {/* First Column: Logo */}
-        <div className="flex justify-center md:justify-end mb-8 md:mb-0 md:mr-8">
-          <Image src="/wardogboxing.png" alt="Wardog Boxing Logo" width={200} height={200} />
-        </div>
-        {/* Second Column: Contact Info */}
+      <div className="relative container mx-auto flex flex-col items-center justify-center px-4">
         <div className="text-center md:text-left">
-          <h2 className="text-3xl font-extrabold mb-4 transition-all duration-300 transform hover:scale-105">CONTACT US</h2>
-          <p className="mb-2 font-bold transition-all duration-300 transform hover:scale-105">GIVE US A CALL</p>
-          <p className="text-lg font-semibold mb-4 transition-all duration-300 transform hover:scale-105">+1 (659) 901-9298</p>
-          <p className="mb-2 font-bold transition-all duration-300 transform hover:scale-105">SEND AN EMAIL</p>
-          <p className="text-lg font-semibold transition-all duration-300 transform hover:scale-105">wardogboxingllc@gmail.com</p>
+        <h2 className="text-white text-4xl md:text-6xl font-extrabold mb-8 transition-all duration-300 transform hover:scale-105">
+            CONTACT US
+          </h2>
+          <p className="mb-2 font-bold transition-all duration-300 transform hover:scale-105">
+            GIVE US A CALL
+          </p>
+          <p className="text-lg font-semibold mb-4 transition-all duration-300 transform hover:scale-105">
+            +1 (659) 901-9298
+          </p>
+          <p className="mb-2 font-bold transition-all duration-300 transform hover:scale-105">
+            SEND AN EMAIL
+          </p>
+          <p className="text-lg font-semibold transition-all duration-300 transform hover:scale-105">
+            wardogboxingllc@gmail.com
+          </p>
         </div>
       </div>
     </section>
@@ -38,5 +42,6 @@ const ContactUs: React.FC = () => {
 };
 
 export default ContactUs;
+
 
 
--- pages/FreeTrial/index.tsx
+++ pages/FreeTrial/index.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 const FreeTrial: React.FC = () => {
   return (
     <section className="relative text-blue-500 py-16 min-h-screen flex flex-col items-center justify-center bg-cover bg-center">
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 h-full">
         <Image 
           src="/slide7.png" 
           alt="Background Image" 
           fill={true} 
-          style={{objectFit:"cover"}} 
+          style={{objectFit:"cover", objectPosition: "center top"}} 
           quality={50} 
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           placeholder="blur"
@@ -21,7 +20,7 @@ const FreeTrial: React.FC = () => {
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
       <div className="relative z-10 text-center max-w-3xl mx-auto px-4">
-        <h1 className="text-4xl md:text-5xl font-bold mb-8 text-white">JOIN US FOR A FREE SESSION</h1>
+        <h1 className="text-4xl md:text-5xl font-bold mb-8 text-white transition-all duration-300 transform hover:scale-105">JOIN US FOR A FREE SESSION</h1>
         <div className="bg-white p-8 shadow-xl rounded-xl text-center transition-all duration-300 transform hover:scale-105 flex flex-col justify-between max-w-lg mx-auto">
           <div className="flex-grow">
             <h2 className="text-black text-2xl md:text-3xl font-bold mb-4">FREE SESSION</h2>
@@ -39,5 +38,6 @@ const FreeTrial: React.FC = () => {
 };
 
 export default FreeTrial;
+
 
 
