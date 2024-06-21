76d73180d6a1bd7da1ea676d7152e58757310fbb
investgod <110871245+investgod@users.noreply.github.com>
2024-06-21 09:21:06 -0500

    GA Connected

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,12 @@ import "@/styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"; 
 config.autoAddCss = false;
+import Analytics from '../components/Analytics';
  
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Analytics />
     <Component {...pageProps} />
     </Layout>  
   )
